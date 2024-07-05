17590ac2f2ccca470961652d363ad47d3a697813
jnkarim <your-email@example.com>
2024-07-05 17:15:42 +0600

    rebase

--- src/components/constants/index.js
+++
@@ -1,8 +0,0 @@
-export const navItems = [
-  { name: "Home", href: "/" },
-  { name: "Contests", href: "/contest" },
-  { name: "Profile", href: "/profile" },
-  { name: "Blogs", href: "/blogs" },
-  { name: "Log in", href: "/login" },
-
-];
